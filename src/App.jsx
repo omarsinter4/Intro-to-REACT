67ed1487f6fcd91de90b5ff22464c053be03c8fc
omarsinter4 <omarsinter4@hotmail.com>
2024-06-18 11:08:50 -0500

    Task accomplish in Tab2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "../public/vite.svg";
 
-import "./App.css";
-<div>
-  <a href="https://vitejs.dev" target="_blank">
-    <img src={viteLogo} className="logo" alt="Vite logo" />
-  </a>
-  <a href="https://react.dev" target="_blank">
-    <img src={reactLogo} className="logo react" alt="React logo" />
-  </a>
-</div>;
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      {puppies.map((puppy) => (
+        <p key={puppy.id}> {puppy.name} </p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
